006e4c36d0da5ba657e07d24cf3fd9fbc16c6c06
dzakwanIrfan <lexdani368@gamil.com>
2023-12-05 17:20:20 +0700

    menambahkan update-user page

---
+++ layouts/sidebar.php
@@ -0,0 +1,57 @@
+<style>
+    *{
+        margin: 0;
+        padding: 0;
+        font-family: 'Poppins', sans-serif;
+    }
+    .sidebar{
+        bottom: 0;
+        display: flex;
+        flex-direction: column;
+        height: 100vh;
+        background-color: #445D48;
+        width: 5rem;
+        text-align: center;
+        position: absolute;
+        position: fixed;
+        align-items: center;
+    }
+
+    nav{
+        margin-top: 3rem;
+        align-items: center;
+    }
+
+    a {
+        width: 1.5rem;
+        height: 1.5rem;
+        display: inline;
+        color: #ffffff;
+        margin-bottom: 1rem;
+        background-color: transparent;
+        border-radius: 50%;
+        display: inline-block;
+        padding: 0.5rem;
+        transition: background-color 0.3s ease;
+        text-decoration: none;
+    }
+
+    nav a:hover {
+        background-color: #8A9E8D;
+    }
+
+    .sidebar .logout{
+        background-color: #DF1616;
+        margin-top: 25rem;
+    }
+</style>
+
+
+<div class="sidebar">
+    <nav>
+        <a href=""><ion-icon name="home-outline"></ion-icon></a>
+        <a href=""><ion-icon name="add-outline"></ion-icon></a>
+        <a href=""><ion-icon name="person-outline"></ion-icon></a>
+    </nav>
+    <a href="" class="logout"><ion-icon name="log-out-outline"></ion-icon></a>
+</div>
--- update-user.php
+++ update-user.php
@@ -4,10 +4,53 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ubah Profil</title>
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="assets/css/update-profil.css">
     <?php include('layouts/font.php') ?>
 </head>
 <body>
-    
+    <?php include('layouts/sidebar.php'); ?>
+    <div class="container-update-user">
+        <h1>Ubah Profil</h1>
+        <div class="wrap">
+            <form action="">
+                <img src="assets/images/ganteng.png" alt="">
+                <input type="file" style="display: none" name="foto">
+                <div class="input-group">
+                    <label for="username">Nama Pengguna</label>
+                    <input type="text" id="username" placeholder="Masukan nama pengguna Anda..">
+                </div>
+                <div class="input-group">
+                    <label for="nama">Nama</label>
+                    <input type="text" id="nama" placeholder="Masukan nama Anda..">
+                </div>
+                <div class="input-group">
+                    <label for="telepon">Nomor Telepon</label>
+                    <input type="number" id="telepon" placeholder="Masukan nomor telepon Anda..">
+                </div>
+                <div class="input-group">
+                    <label for="alamat">Alamat</label>
+                    <input type="text" id="alamat" placeholder="Masukan alamat Anda..">
+                </div>
+                <div class="dropdown-group">
+                    <div class="input-group">
+                        <label for="provinsi">Provinsi</label>
+                        <select name="provinsi" id="provinsi">
+                            <option value="">Pilih Provinsi</option>
+                        </select>
+                    </div>
+                    <div class="input-group">
+                        <label for="kota">Kota</label>
+                        <select name="kota" id="kota">
+                            <option value="">Pilih Kota</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="submit-group">
+                    <a href="" class="batal">Batal</a>
+                    <input type="submit" value="Ubah" class="submit">
+                </div>
+            </form>
+        </div>
+    </div>
 </body>
 </html>
