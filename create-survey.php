8cb801f745194c2df20ed7df90e7c187bf212f68
dzakwanIrfan <lexdani368@gamil.com>
2023-12-13 06:39:58 +0700

    menambahkan page create-survey

---
+++ create-survey.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Buat Survei - GWS</title>
+    <?php include("layouts/font.php"); ?>
+    <link rel="stylesheet" href="assets/css/create-survey.css">
+</head>
+<body>
+    <?php include("layouts/sidebar.php"); ?>
+    <div class="container">
+        <div class="judul">Kelola Survei</div>
+        <form action="#">
+            <div class="input-container">
+                <div class="input-group">
+                    <label for="judul">Judul Survei</label>
+                    <input type="text" id="judul" placeholder="Masukan judul survei Anda ...">
+                </div>
+                <div class="input-group">
+                    <label for="desk">Deskripsi Survei</label>
+                    <textarea name="desk" id="desk" cols="30" rows="5" placeholder="Masukan deskripsi survei Anda ..."></textarea>
+                </div>
+                <div class="input-group">
+                    <label for="gambar">Gambar Survei</label>
+                    <img src="assets/images/gambar-survey.png" alt="survei">
+                    <input type="file" id="gambar" class="file">
+                </div>
+            </div>
+            <input type="submit" value="Buat Survei">
+        </form>
+    </div>
+</body>
+</html>
--- layouts/sidebar.php
+++ layouts/sidebar.php
@@ -50,7 +50,7 @@
 <div class="sidebar">
     <nav>
         <a href="index.php"><ion-icon name="home-outline"></ion-icon></a>
-        <a href=""><ion-icon name="add-outline"></ion-icon></a>
+        <a href="create-survey.php"><ion-icon name="add-outline"></ion-icon></a>
         <a href=""><ion-icon name="person-outline"></ion-icon></a>
     </nav>
     <a href="" class="logout"><ion-icon name="log-out-outline"></ion-icon></a>
