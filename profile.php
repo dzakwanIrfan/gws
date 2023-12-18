81663cb405df1eec00af1cd0eda58198f484dfaa
katsuku27 <148852467+katsuku27@users.noreply.github.com>
2023-12-18 11:51:04 +0700

    profile and create survey link to question

--- profile.php
+++ profile.php
@@ -1,3 +1,25 @@
+<?php 
+    include 'conection.php';
+
+    session_start();
+
+    if(!$_SESSION['username']){
+         header('Location:login.php');
+    }
+    
+    $id=$_SESSION['id_pengguna'];
+
+    $query="SELECT survei.*, pengguna.* FROM survei JOIN pengguna ON survei.id_pengguna=pengguna.id_pengguna WHERE survei.id_pengguna='$id'";
+    $result=mysqli_query($conn,$query);
+    $row=mysqli_fetch_array($result);
+
+    if($row['foto_pengguna']!=''){
+        $image=$row['foto_pengguna'];
+    }else{
+        $image='assets/images/profile-picture.png';
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,51 +35,37 @@
         <div class="container-profile">
             <h1>Profil</h1>
             <div class="bio">
-                <img src="assets/images/profile-picture.png" alt="">
-                <div class="nama">Nama Pengguna</div>
-                <div class="username">username</div>
+                <img src="<?php echo $image ?>" alt="">
+                <div class="nama"><?php echo $row['nama_pengguna']?></div>
+                <div class="username"><?php echo $row['namaUser_pengguna']?></div>
             </div>
             <div class="biodata">
-                <div class="isi-biodata">name@gmail.com</div>
-                <div class="isi-biodata">Laki-laki</div>
+                <div class="isi-biodata"><?php echo $row['email_pengguna']?></div>
+                <div class="isi-biodata">
+                    <?php 
+                        if($row['jenisKelamin_pengguna']==''){
+                            echo 'belom mengisi jenis kelamin';
+                        }else{
+                            echo $row['jenisKelamin_pengguna'];
+                        }
+                    ?>
+                </div>
             </div>
             <div class="ubah-bio"><a href="update-profile.php">Ubah</a></div>
         </div>
         <div class="container-riwayat">
             <div class="riwayat-judul">Riwayat Pembuatan Survey</div>
             <div class="riwayat">
-                <a href="#" class="riwayat-group">
-                    <div class="judul-riwayat">Judul Survei</div>
-                    <div class="waktu-riwayat">1 December 2023</div>
-                </a>
-                <a href="#" class="riwayat-group">
-                    <div class="judul-riwayat">Judul Survei</div>
-                    <div class="waktu-riwayat">1 December 2023</div>
-                </a>
-                <a href="#" class="riwayat-group">
-                    <div class="judul-riwayat">Judul Survei</div>
-                    <div class="waktu-riwayat">1 December 2023</div>
-                </a>
-                <a href="#" class="riwayat-group">
-                    <div class="judul-riwayat">Judul Survei</div>
-                    <div class="waktu-riwayat">1 December 2023</div>
-                </a>
-                <a href="#" class="riwayat-group">
-                    <div class="judul-riwayat">Judul Survei</div>
-                    <div class="waktu-riwayat">1 December 2023</div>
-                </a>
-                <a href="#" class="riwayat-group">
-                    <div class="judul-riwayat">Judul Survei</div>
-                    <div class="waktu-riwayat">1 December 2023</div>
-                </a>
-                <a href="#" class="riwayat-group">
-                    <div class="judul-riwayat">Judul Survei</div>
-                    <div class="waktu-riwayat">1 December 2023</div>
-                </a>
-                <a href="#" class="riwayat-group">
-                    <div class="judul-riwayat">Judul Survei bawah</div>
-                    <div class="waktu-riwayat">1 December 2023</div>
-                </a>
+                <?php 
+                    while($row=mysqli_fetch_array($result)){
+                        ?>
+                        <a href="survey.php?id=<?php echo $row['id_survei'] ?>" class="riwayat-group">
+                            <div class="judul-riwayat"><?php echo $row['judul_survei'] ?></div>
+                            <div class="waktu-riwayat"><?php echo $row['waktu_survei']?></div>
+                        </a>
+                        <?php
+                    } 
+                ?>
             </div>
         </div>
     </div>
