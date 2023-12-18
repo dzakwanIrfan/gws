c2d81967f77cf4c7bdd10e5e43a643f9de30f627
Dzakwan Irfan <lexdani368@gmail.com>
2023-12-18 20:03:21 +0700

    membuat page laporan

---
+++ survey-laporan.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laporan Survey</title>
+    <?php include("layouts/font.php") ?>
+    <link rel="stylesheet" href="assets/css/survey-laporan.css">
+</head>
+<body>
+    <?php include("layouts/sidebar.php") ?>
+    <div class="container">
+        <div class="header">
+            <div class="judul">Judul Survei</div>
+            <div class="deskripsi">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum quibusdam inventore, deleniti rerum sapiente facere aliquid enim nisi consequatur veniam laudantium saepe praesentium illum? Quae fugiat ullam officia facilis ipsa.</div>
+        </div>
+
+        <div class="laporan-container">
+            <div class="laporan">
+                <div class="laporan-pertanyaan">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae facilis, a corporis expedita sapiente sit. Laboriosam illum perspiciatis aperiam, quis fugiat veniam. Nemo fugiat repellat consectetur veritatis molestias id, sequi minima ipsum, illum earum rerum saepe quod, sunt voluptates quaerat?</div>
+                <div class="laporan-opsi">
+                    <div class="grup-opsi">
+                        <div class="label-opsi">
+                            <div class="opsi">Lorem, ipsum.</div>
+                            <div class="opsi-jumlah"><span>85</span> suara</div>
+                        </div>
+                        <div class="persen-opsi">
+                            <div class="batang" style="width: 85%;" ></div>
+                        </div>
+                    </div>
+                    <div class="grup-opsi">
+                        <div class="label-opsi">
+                            <div class="opsi">Lorem, ipsum.</div>
+                            <div class="opsi-jumlah"><span>15</span> suara</div>
+                        </div>
+                        <div class="persen-opsi">
+                            <div class="batang" style="width: 15%;" ></div>
+                        </div>
+                    </div>
+                    <div class="grup-opsi">
+                        <div class="label-opsi">
+                            <div class="opsi">Lorem, ipsum.</div>
+                            <div class="opsi-jumlah"><span>24</span> suara</div>
+                        </div>
+                        <div class="persen-opsi">
+                            <div class="batang" style="width: 24%;" ></div>
+                        </div>
+                    </div>
+                    <div class="grup-opsi">
+                        <div class="label-opsi">
+                            <div class="opsi">Lorem, ipsum.</div>
+                            <div class="opsi-jumlah"><span>65</span> suara</div>
+                        </div>
+                        <div class="persen-opsi">
+                            <div class="batang" style="width: 65%;" ></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="laporan">
+                <div class="laporan-pertanyaan">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae facilis, a corporis expedita sapiente sit. Laboriosam illum perspiciatis aperiam, quis fugiat veniam. Nemo fugiat repellat consectetur veritatis molestias id, sequi minima ipsum, illum earum rerum saepe quod, sunt voluptates quaerat?</div>
+                <div class="laporan-opsi">
+                    <div class="grup-opsi">
+                        <div class="label-opsi">
+                            <div class="opsi">Lorem, ipsum.</div>
+                            <div class="opsi-jumlah"><span>85</span> suara</div>
+                        </div>
+                        <div class="persen-opsi">
+                            <div class="batang" style="width: 85%;" ></div>
+                        </div>
+                    </div>
+                    <div class="grup-opsi">
+                        <div class="label-opsi">
+                            <div class="opsi">Lorem, ipsum.</div>
+                            <div class="opsi-jumlah"><span>15</span> suara</div>
+                        </div>
+                        <div class="persen-opsi">
+                            <div class="batang" style="width: 15%;" ></div>
+                        </div>
+                    </div>
+                    <div class="grup-opsi">
+                        <div class="label-opsi">
+                            <div class="opsi">Lorem, ipsum.</div>
+                            <div class="opsi-jumlah"><span>24</span> suara</div>
+                        </div>
+                        <div class="persen-opsi">
+                            <div class="batang" style="width: 24%;" ></div>
+                        </div>
+                    </div>
+                    <div class="grup-opsi">
+                        <div class="label-opsi">
+                            <div class="opsi">Lorem, ipsum.</div>
+                            <div class="opsi-jumlah"><span>65</span> suara</div>
+                        </div>
+                        <div class="persen-opsi">
+                            <div class="batang" style="width: 65%;" ></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
