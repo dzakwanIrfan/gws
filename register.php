35d3e3c65bb34c407d85834a99b4ad65f8dd35e0
katsuku27 <148852467+katsuku27@users.noreply.github.com>
2023-12-26 07:24:46 +0700

    add pop up

--- register.php
+++ register.php
@@ -51,6 +51,40 @@
     <title>Register</title>
     <link rel="stylesheet" href="assets/css/loginRegister.css">
     <?php include('layouts/font.php') ?>
+    <style>
+        .overlay {
+        display: none;
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, 0.5);
+        justify-content: center;
+        align-items: center;
+      }
+
+      .popup {
+        background-color: #fff;
+        padding: 20px;
+        max-width: 600px;
+        margin: 20px auto;
+        border-radius: 8px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+      }
+
+      .close-btn {
+        cursor: pointer;
+        color: #333;
+        float: right;
+        font-size: 20px;
+      }
+
+      #submitButton:disabled {
+            background-color: #ccc;
+            cursor: not-allowed;
+        }
+    </style>
 </head>
 <body>
     <div class="container" id="register">
@@ -69,7 +103,7 @@
                     echo "<div class='eror-popup'><span>Gagal terdaftar mohon isi kembali!</span></div>";
                 }
             ?>
-            <form action="" method="post">
+            <form action="" method="post" onsubmit="return validateForm()">
                 <div class="input-group">
                     <label for="name">Nama</label>
                     <input type="text" name="name" id="name" placeholder="Masukan nama Anda.." required>
@@ -90,7 +124,11 @@
                     <label for="passwordku">Konfirmasi Kata Sandi</label>
                     <input type="password" name="confirm" id="konfirmasiku" placeholder="Masukan password Anda.." required>
                 </div>
-                <input type="submit" name="register" value="Daftar" class="submit" required>
+                <label for="termsCheckbox">
+                    <a href="#" onclick="showPopup()">agree to the terms and conditions</a>
+                    <input type="checkbox" id="termsCheckbox" onchange="enableSubmit()" />
+                </label>
+                <input type="submit" name="register" value="Daftar" id="submitButton" class="submit" disabled>
             </form>
             <p class="atau">- ATAU -</p>
             <small>Sudah memiliki akun? <a href="login.php"><b>Masuk</b></a></small>
@@ -100,5 +138,67 @@
             <p>Glorious Web Survey</p>
         </div>
     </div>
+
+    <div id="overlay" class="overlay">
+      <div class="popup">
+        <span class="close-btn" onclick="hidePopup()">&times;</span>
+        <h2>Terms and Conditions</h2>
+        <p>
+            Selamat datang di GWS! Mohon diperhatikan bahwa dengan mengakses dan menggunakan GWS, Anda dianggap menyetujui syarat dan ketentuan penggunaan berikut. Jika Anda tidak setuju dengan syarat dan ketentuan ini, harap untuk tidak menggunakan situs ini. <br>
+            1. penggunaan Materi <br>
+            Semua materi, informasi, dan konten lainnya yang terdapat di GWS dimiliki atau dikendalikan oleh Dzakwan. Pengguna tidak diperbolehkan mengunduh, menggandakan, memodifikasi, atau mendistribusikan materi tersebut tanpa izin tertulis dari Dzakwan. <br>
+            <br>
+            2. Akun Pengguna <br>
+            Jika Anda membuat akun di GWS, Anda bertanggung jawab untuk menjaga keamanan informasi akun Anda dan memberikan informasi yang benar dan akurat. Anda juga bertanggung jawab atas semua kegiatan yang terjadi di bawah akun Anda. <br>
+            <br>
+            3. Batasan Tangung Jawab <br>
+            Dzakwan tidak bertanggung jawab atas kerugian atau kerusakan yang timbul akibat penggunaan atau akses terhadap GWS. Kami tidak memberikan jaminan atas keakuratan atau kelengkapan materi di situs ini. <br>
+            <br>
+            4. Hak Cipta <br>
+            Semua hak cipta dan hak kekayaan intelektual lainnya terkait dengan GWS dan kontennya adalah milik Dzakwan. Pengguna tidak diperbolehkan menggunakan, mengubah, atau mendistribusikan konten tanpa izin tertulis. <br>
+            <br>
+            5. Perubahan pada Syarat dan Ketentuan <br>
+            Dzakwan berhak untuk mengubah syarat dan ketentuan ini setiap saat tanpa pemberitahuan sebelumnya. Perubahan akan berlaku sejak saat diumumkan di situs ini. <br>
+            <br>
+            6. Hukum yang Berlaku <br>
+            Syarat dan ketentuan ini diatur oleh hukum yang berlaku di Purbalingga dan setiap sengketa yang timbul akan diselesaikan di pengadilan yang berwenang di Purbalingga. <br>
+            <br>
+            7. Konten yang Diunggah Pengguna <br>
+            a. Pengguna dapat mengunggah survei dan jawaban survei ke GWS, dan dengan melakukan itu, mereka setuju bahwa mereka bertanggung jawab sepenuhnya atas survei dan jawaban atas survei  yang diunggah. Dzakwan tidak bertanggung jawab atas keakuratan, legalitas, atau kepatutan konten yang diunggah oleh pengguna. <br>
+            b. Dzakwan berhak, tanpa pemberitahuan sebelumnya, untuk menghapus atau mengubah konten yang dianggap mengandung unsur rasisme, seksual, kepentingan politik, ujaran kebencian, kegiatan ilegal, kriminalitas atau melanggar hukum yang berlaku. <br>
+            <br>
+            8. Lisensi Penggunaan Konten <br>
+            a. Dengan mengunggah survei dan jawaban survei ke GWS, pengguna memberikan Dzakwan lisensi non-eksklusif, bebas royalti, dapat dipindahtangankan, dan dapat dilisensikan untuk menggunakan, mereproduksi, mengadaptasi, mendistribusikan, dan menampilkan konten tersebut di bawah kerangka hukum yang berlaku. <br>
+            b. Pengguna memahami dan setuju bahwa survei dan jawaban survei yang diunggah dapat diakses oleh pengguna lain dan dapat digunakan oleh GWS untuk tujuan promosi dan pemasaran. <br>
+            <br>
+            9. Tanggung Jawab Hukum Pengguna <br>
+            Pengguna bertanggung jawab penuh atas konten yang diunggah dan setuju untuk melepaskan Dzakwan dari segala tuntutan atau kerugian yang timbul akibat konten yang diunggah. <br>
+            <br>
+            Terima kasih atas kunjungan Anda di GWS!
+        </p>
+      </div>
+    </div>
+    <script>
+      function showPopup() {
+        document.getElementById("overlay").style.display = "flex";
+      }
+
+      function hidePopup() {
+        document.getElementById("overlay").style.display = "none";
+      }
+
+      function enableSubmit() {
+        document.getElementById("submitButton").disabled =
+          !document.getElementById("termsCheckbox").checked;
+      }
+
+      function validateForm() {
+        if (!document.getElementById("termsCheckbox").checked) {
+          alert("Please agree to the terms and conditions before submitting.");
+          return false;
+        }
+        return true;
+      }
+    </script>
 </body>
 </html>
